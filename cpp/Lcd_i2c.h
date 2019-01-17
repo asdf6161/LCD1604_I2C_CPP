c325321a71e1dd8d1c7f09e33bbbf3f058279252
yura <sk12.yura@gmail.com>
2019-01-18 01:27:09 +0700

    added empty class Lcd_i2c

---
+++ cpp/Lcd_i2c.h
@@ -0,0 +1,21 @@
+/*
+ * Lcd_i2c.h
+ *
+ *  Created on: 18 џэт. 2019 у.
+ *      Author: yura
+ */
+
+#ifndef LCD_I2C_H_
+#define LCD_I2C_H_
+
+namespace lcd {
+
+class Lcd_i2c {
+public:
+	Lcd_i2c();
+	virtual ~Lcd_i2c();
+};
+
+} /* namespace lcd */
+
+#endif /* LCD_I2C_H_ */
