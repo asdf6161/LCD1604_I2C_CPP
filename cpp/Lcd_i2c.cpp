76196e2f1273def2418d5fc05bd7c76f55c3a601
yura <sk12.yura@gmail.com>
2019-01-18 13:29:27 +0700

    added class Lcd_i2c

---
+++ cpp/Lcd_i2c.cpp
@@ -0,0 +1,21 @@
+/*
+ * Lcd_i2c.cpp
+ *
+ *  Created on: 18 џэт. 2019 у.
+ *      Author: yura
+ */
+
+#include <Lcd_i2c.h>
+
+namespace lcd {
+
+Lcd_i2c::Lcd_i2c() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Lcd_i2c::~Lcd_i2c() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace lcd */
