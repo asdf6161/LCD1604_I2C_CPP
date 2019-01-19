1a889d820994970356c92242bc7697af42dc8967
yura <sk12.yura@gmail.com>
2019-01-20 02:50:01 +0700

    added class inherit abstract class for send, and are descripted some methods

---
+++ cpp/Pcf8574t.cpp
@@ -0,0 +1,56 @@
+/*
+ * Pcf8574t.cpp
+ *
+ *  Created on: 20 џэт. 2019 у.
+ *      Author: yura
+ */
+
+#ifdef USE_FULL_LL_DRIVER
+
+#include <Pcf8574t.h>
+
+namespace pcf {
+
+Pcf8574t::Pcf8574t(uint8_t addr, pcf_func_out f_out, pcf_func_in f_in) {
+	this->f_in = f_in;
+	this->f_out = f_out;
+	this->addr = addr;
+	this->packet = {0,0,0,0,0,0,0,0};
+}
+
+Pcf8574t::~Pcf8574t() {
+
+}
+
+void Pcf8574t::send_half_byte(const uint8_t half_bt){
+	this->__send_half_byte_with_strob(half_bt);
+}
+
+void Pcf8574t::send_full_byte(const uint8_t bt){
+
+}
+
+void Pcf8574t::__send_half_byte_with_strob(const uint8_t half_bt){
+	this->packet.byte &= 0b1111;
+	this->packet.byte |= half_bt << 4;
+	this->packet.bits.EN = 0;
+	this->f_out(this->addr, this->packet.byte);
+	this->packet.bits.EN = 1;
+	this->f_out(this->addr, this->packet.byte);
+}
+
+void Pcf8574t::__set_pin(pcf_packet *p){
+	this->f_out(this->addr, p->byte);
+}
+
+void Pcf8574t::enable_led(const bool state){
+	if (state)
+		this->packet.bits.LED = 1;
+	else
+		this->packet.bits.LED = 0;
+	this->__set_pin(&this->packet);
+}
+
+} /* namespace lcd */
+
+#endif // USE_FULL_LL_DRIVER2
---
+++ cpp/Pcf8574t.h
@@ -0,0 +1,80 @@
+/*
+ * Pcf8574t.h
+ *
+ *  Created on: 20 џэт. 2019 у.
+ *      Author: yura
+ */
+
+#ifdef USE_FULL_LL_DRIVER
+
+#ifndef PCF8574T_H_
+#define PCF8574T_H_
+
+#include "stdint.h"
+#include "Lcd_sender_abstract.h"
+
+extern "C" {
+#ifdef STM32F303xC
+//#include "stm32f3xx_ll_utils.h"
+#include "stm32f303xc.h"
+#endif
+}
+
+namespace pcf {
+
+// function type for transmit data to lcd
+typedef void (*pcf_func_out) (uint8_t, uint8_t);
+typedef uint8_t (*pcf_func_in) (uint8_t);
+
+typedef union
+{
+  struct
+  {
+    uint8_t RS : 1;		// P0
+    uint8_t RW : 1;		// P1
+    uint8_t EN : 1;		// P2
+    uint8_t LED : 1;	// P3
+    /*half byte*/
+    uint8_t DB4 : 1;	// P4
+    uint8_t DB5 : 1;	// P5
+    uint8_t DB6 : 1;	// P6
+    uint8_t DB7 : 1;	// P7
+    /*half byte*/
+  }bits;
+  unsigned char byte;
+} pcf_packet;
+
+class Pcf8574t : public Lcd_sender_abstract {
+public:
+	/*
+	 * addr  - address of module Pcf8574t
+	 * f_out - i2c func for send data to Pcf8574t
+	 * f_in  - i2c func for recv data from Pcf8574t
+	 * */
+	Pcf8574t(uint8_t addr, pcf_func_out f_out, pcf_func_in f_in);
+	virtual ~Pcf8574t();
+
+// methods
+public:
+	void send_half_byte(const uint8_t half_bt);
+	void send_full_byte(const uint8_t bt);
+	void enable_led(const bool state);
+
+// PV methods
+private:
+	void __send_half_byte_with_strob(const uint8_t half_bt);
+	void __set_pin(pcf_packet *p);
+
+// vars
+private:
+	pcf_func_out f_out;
+	pcf_func_in f_in;
+	uint8_t addr;
+	pcf_packet packet;
+
+};
+
+} /* namespace lcd */
+
+#endif /* PCF8574T_H_ */
+#endif // USE_FULL_LL_DRIVER2
